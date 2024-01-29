efd894438797ee731eb9269efa5550b2a0ca6970
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-29 18:17:00 +0530

    initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>Todo List</h1>
+    </div>
+  )
+}
+
+export default App
