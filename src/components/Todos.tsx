f948d4cc243d341423625d3b601f4a4622d5e7e2
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-29 18:44:38 +0530

    style: add responsive width

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,15 +3,16 @@ import Todo from "../partials/Todo"
 const Todos = () => {
   return (
     <div className="w-full text-center bg-indigo-100 p-8">
+        <div className="w-5/6 mx-auto">
+          
+          <h1 className='text-4xl font-bold text-slate-800'>Todo List</h1>
+          <TodoInput />
+          <div className="flex mx-auto flex-col gap-2">
+              <Todo />
+              <Todo />
+          </div>
         
-        <h1 className='text-4xl font-bold text-slate-800'>Todo List</h1>
-        
-        <TodoInput />
-
-        <div className="flex flex-col gap-2">
-            <Todo />
-            <Todo />
-        </div>
+        </div>     
     </div>
   )
 }
