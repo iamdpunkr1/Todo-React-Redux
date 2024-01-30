af4b792dc708ad59ad587f0922dd457358e42cb3
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-30 17:20:05 +0530

    feat: add Redux dev tools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import {composeWithDevTools} from '@redux-devtools/extension';
 import todoReducer from './todo/todoReducer';
 
-const store = createStore(todoReducer);
+const store = createStore(todoReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
